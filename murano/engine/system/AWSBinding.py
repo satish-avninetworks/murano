1752450794ef2220a209123bcf4fe50b11045643
Nagaraj <nagraj@avni.io>
2016-03-24 15:33:55 +0530

    cloud enhancements

--- murano/engine/system/AWSBinding.py
+++ murano/engine/system/AWSBinding.py
@@ -30,14 +30,17 @@ class AWSBinding(object):
     def __init__(self,cloud):
         self.cls = get_driver(Provider.EC2)
         #self.driver = self.cls('AKIAIETZPSM636GLXORA','rOLla8GFbmAB16Zi3TMzOMgoeoicrB7BUY0nOmg+', region="us-west-1")
+        import pdb;pdb.set_trace()
         self.driver = self.cls(cloud.user, cloud.key, region="us-east-1")
         self.cloud = cloud
 
     def createnode(self, image,flavor,name):
         #TODO: Remove hardcoding
+        import pdb;pdb.set_trace()
         images = NodeImage(id=image, name=None,driver=self.driver)
         sizes = self.driver.list_sizes()
-        node = self.driver.create_node(name=name,image=images,size=flavor)
+        size = [s for s in sizes if s.id == flavor][0]
+        node = self.driver.create_node(name=name,image=images,size=size)
         # Wait until node is up and running and has IP assigned
         # this blocking call is needed to ensure instance is up
         try:
